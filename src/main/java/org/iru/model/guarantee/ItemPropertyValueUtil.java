07fa8338018be363d4dfffa4eb158126f7cfdff6
Matthieu Verbert <Matthieu.Verbert@iru.org>
2015-09-03 11:01:58 +0200

    Clean up whitespace

--- src/main/java/org/iru/model/guarantee/ItemPropertyValueUtil.java
+++ src/main/java/org/iru/model/guarantee/ItemPropertyValueUtil.java
@@ -8,7 +8,7 @@ public class ItemPropertyValueUtil {
 
 	public static final String VALIDITY_PERIOD_DURATION = "duration";
 	public static final String VALIDITY_PERIOD_FINAL_DATE = "final-date";
-	
+
 	public static final String MONETARY_LIMIT_CURRENCY = "currency";
 	public static final String MONETARY_LIMIT_VALUE = "value";
 
@@ -18,15 +18,15 @@ public class ItemPropertyValueUtil {
 		}
 		throw new IllegalArgumentException(value);
 	}
-	
+
 	public String anchorValue(String value, ItemPropertyCodeType code) {
 		return substring(value, code, '#');
 	}
-	
+
 	public String withAnchorValue(String anchorValue, ItemPropertyCodeType code) {
 		return code.value() + "#" + anchorValue;
 	}
-	
+
 	public Map<String, String> queryParameters(String value, ItemPropertyCodeType code) {
 		String paramsString = substring(value, code, '?');
 		String[] params = paramsString.split("&");
@@ -41,15 +41,15 @@ public class ItemPropertyValueUtil {
 				result.put(param, null);
 			}
 		}
-		
+
 		return result;
 	}
-	
+
 	public String withQueryParameters(Map<String,String> parameters, ItemPropertyCodeType code) {
 		StringBuilder sb = new StringBuilder(code.value());
 		if (parameters != null && ! parameters.isEmpty()) {
 			boolean first = true;
-			
+
 			for (Map.Entry<String, String> param : parameters.entrySet()) {
 				sb.append(first ? "?" : "&");
 				sb.append(param.getKey());
@@ -61,27 +61,27 @@ public class ItemPropertyValueUtil {
 		}
 		return sb.toString();
 	}
-	
+
 	public String withQueryParameter(String name, String value, ItemPropertyCodeType code) {
 		return withQueryParameters(Collections.singletonMap(name, value), code);
 	}
-	
+
 	public String rulesetValue(String value) {
 		return anchorValue(value, ItemPropertyCodeType.USAGE_RULESET);
 	}
-	
+
 	public String withRulesetValue(String value) {
 		return withAnchorValue(value, ItemPropertyCodeType.USAGE_RULESET);
 	}
-	
+
 	public String formatValue(String value) {
 		return anchorValue(value, ItemPropertyCodeType.FORMAT);
 	}
-	
+
 	public String withFormatValue(String value) {
 		return withAnchorValue(value, ItemPropertyCodeType.FORMAT);
 	}
-	
+
 	public String withFormatValue(FormatTypeAnchor value) {
 		return withFormatValue(value.value());
 	}
@@ -93,11 +93,11 @@ public class ItemPropertyValueUtil {
 	public String withFormatElectronicValue() {
 		return withFormatValue(FormatTypeAnchor.ELECTRONIC);
 	}
-	
+
 	public Map.Entry<String, String> validityPeriodParameter(String value) {
 		return queryParameters(value, ItemPropertyCodeType.VALIDITY_PERIOD).entrySet().iterator().next();
 	}
-	
+
 	public String withValidityPeriodParameter(String name, String value) {
 		return withQueryParameter(name, value, ItemPropertyCodeType.VALIDITY_PERIOD);
 	}
@@ -105,7 +105,7 @@ public class ItemPropertyValueUtil {
 	public String withValidityPeriodDurationParameter(String value) {
 		return withValidityPeriodParameter(VALIDITY_PERIOD_DURATION, value);
 	}
-	
+
 	public String withValidityPeriodFinalDateParameter(String value) {
 		return withValidityPeriodParameter(VALIDITY_PERIOD_FINAL_DATE, value);
 	}
@@ -113,7 +113,7 @@ public class ItemPropertyValueUtil {
 	public Map<String,String> monetaryLimitParameters(String value) {
 		return queryParameters(value, ItemPropertyCodeType.MONETARY_LIMIT);
 	}
-	
+
 	public String withMonetaryLimitParameters(String currency, int amount) {
 		Map<String,String> p = new LinkedHashMap<String, String>();
 		p.put(MONETARY_LIMIT_VALUE, Integer.toString(amount));
