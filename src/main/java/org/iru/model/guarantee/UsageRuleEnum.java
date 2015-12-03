839672d266f3836ecb85616d57bfb457cdaa59f2
Matthieu Verbert <Matthieu.Verbert@iru.org>
2015-12-03 12:49:12 +0100

    Let's not hard-code the list of possible volet count in an enumeration
    Having the start of the rule URI makes sense though

--- src/main/java/org/iru/model/guarantee/UsageRuleEnum.java
+++ src/main/java/org/iru/model/guarantee/UsageRuleEnum.java
@@ -2,20 +2,17 @@ package org.iru.model.guarantee;
 
 public enum UsageRuleEnum {
     
-    VOLET_COUNT_4("http://www.iru.org/model/guarantee-1/volet#4"),
-    VOLET_COUNT_6("http://www.iru.org/model/guarantee-1/volet#6"),
-    VOLET_COUNT_14("http://www.iru.org/model/guarantee-1/volet#14"),
-    VOLET_COUNT_20("http://www.iru.org/model/guarantee-1/volet#20"),
+    VOLET_COUNT("http://www.iru.org/model/guarantee-1/volet"),
     PILOT("http://www.iru.org/model/association/deed-of-engagement#pilot-addendum");
     
-    private final String URI;
+    private final String uri;
 
-    private UsageRuleEnum(String URI) {
-        this.URI = URI;
+    private UsageRuleEnum(String uri) {
+        this.uri = uri;
     }
 
     public String getURI() {
-        return URI;
+        return uri;
     }
     
 }
