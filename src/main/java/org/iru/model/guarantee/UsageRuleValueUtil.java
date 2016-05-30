58bde84fcc4c00cd04211a9e9692dc36b63a974a
Matthieu Verbert <Matthieu.Verbert@iru.org>
2016-05-30 15:31:28 +0200

    Add addendum information for Tobacco or Tobacco/Alcohol carnets

--- src/main/java/org/iru/model/guarantee/UsageRuleValueUtil.java
+++ src/main/java/org/iru/model/guarantee/UsageRuleValueUtil.java
@@ -3,6 +3,8 @@ package org.iru.model.guarantee;
 public class UsageRuleValueUtil {
 
     public static final String PILOT_ADDENDUM = "pilot-addendum";
+    public static final String TOBACCO_ADDENDUM = "tobacco-addendum";
+    public static final String TOBACCOALCOHOL_ADDENDUM = "tobaccoalcohol-addendum";
     
     public String withVoletCount(short voletCount) {
         return withAnchorValue(Short.toString(voletCount), UsageRule.VOLET_COUNT);
@@ -11,6 +13,15 @@ public class UsageRuleValueUtil {
     public String withPilotAddendum() {
         return withAnchorValue(PILOT_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
     }
+
+    public String withTobaccoAddendum() {
+        return withAnchorValue(TOBACCO_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
+    }
+
+    public String withTobaccoAlcoholAddendum() {
+        return withAnchorValue(TOBACCOALCOHOL_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
+    }
+
     
     public String withAnchorValue(String anchorValue, UsageRule usageRule) {
         return usageRule.getURI() + "#" + anchorValue;
