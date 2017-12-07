6bc88cba09239d30394110d2f5d1ea6c1defcf85
Matthieu Verbert <Matthieu.Verbert@iru.org>
2017-12-07 16:43:44 +0100

    Add test to verify list TIR contracting and operational countries
    It's a lightweight test as we only verify the overall count of countries
    in each category. But it will catch most obvious errors.

---
+++ src/test/java/org/iru/model/iso_3166_1/CountryEnumsTest.java
@@ -0,0 +1,48 @@
+package org.iru.model.iso_3166_1;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CountryEnumsTest {
+	
+	private static int TIR_CONTRACTING_PARTIES = 71; //https://www.unece.org/tir/welcome.html
+	private static int TIR_CP_COUNTRIES = TIR_CONTRACTING_PARTIES - 1 ; // EU is not a country
+	
+	private static String[] NON_OPERATIONAL_COUNTRIES = { // https://www.unece.org/tir/system/countries.html
+			"Algeria",
+			"Canada",
+			"Chile",
+			"China",
+			"India",
+			"Indonesia",
+			"Liberia",
+			"Pakistan",
+			"Republic of Korea",
+			"United States of America",
+			"Uruguay"
+	};
+	
+	private static int TIR_OPERATIONAL_COUNTRIES = TIR_CP_COUNTRIES - NON_OPERATIONAL_COUNTRIES.length; 
+
+	@Test
+	public void checkISO2CPList() {
+		Assert.assertEquals(TIR_CP_COUNTRIES, org.iru.model.iso_3166_1_alpha_2.ContractingPartyCountryCodeType.values().length);
+	}
+	
+	@Test
+	public void checkISO3CPList() {
+		Assert.assertEquals(TIR_CP_COUNTRIES, org.iru.model.iso_3166_1_alpha_3.ContractingPartyCountryCodeType.values().length);
+	}
+
+	@Test
+	public void checkISO2OpList() {
+		Assert.assertEquals(TIR_OPERATIONAL_COUNTRIES, org.iru.model.iso_3166_1_alpha_2.TIROperationalCountryCodeType.values().length);
+	}
+	
+	@Test
+	public void checkISO3OpList() {
+		Assert.assertEquals(TIR_OPERATIONAL_COUNTRIES, org.iru.model.iso_3166_1_alpha_3.TIROperationalCountryCodeType.values().length);
+	}
+
+	
+}
