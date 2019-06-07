2c5bae54aeb27ba88ed2d82b5f99d99297ac6529
Matthieu Verbert <Matthieu.Verbert@iru.org>
2019-06-07 12:16:14 +0200

    TINT-2537: improve helpers for UsageRule handling

--- src/main/java/org/iru/model/guarantee/UsageRuleValueUtil.java
+++ src/main/java/org/iru/model/guarantee/UsageRuleValueUtil.java
@@ -2,29 +2,38 @@ package org.iru.model.guarantee;
 
 public class UsageRuleValueUtil {
 
-    public static final String PILOT_ADDENDUM = "pilot-addendum";
-    public static final String TOBACCO_ADDENDUM = "tobacco-addendum";
-    public static final String TOBACCOALCOHOL_ADDENDUM = "tobaccoalcohol-addendum";
-    
-    public String withVoletCount(short voletCount) {
+    private static final String _ADDENDUM = "-addendum";
+
+	public String withVoletCount(short voletCount) {
         return withAnchorValue(Short.toString(voletCount), UsageRule.VOLET_COUNT);
     }
     
-    public String withPilotAddendum() {
-        return withAnchorValue(PILOT_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
+    public String withAddendum(String subusageItemIdentificationPart) {
+    	return withAnchorValue(subusageItemIdentificationPart+_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
     }
-
-    public String withTobaccoAddendum() {
-        return withAnchorValue(TOBACCO_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
-    }
-
-    public String withTobaccoAlcoholAddendum() {
-        return withAnchorValue(TOBACCOALCOHOL_ADDENDUM, UsageRule.DEED_OF_ENGAGEMENT);
-    }
-
     
     public String withAnchorValue(String anchorValue, UsageRule usageRule) {
         return usageRule.getURI() + "#" + anchorValue;
     }
+    
+    private String substring(String value, UsageRule rule, char separator) {
+		if (value.startsWith(rule.getURI() + separator)) {
+			return value.substring(rule.getURI().length() + 1);
+		}
+		throw new IllegalArgumentException(value);
+	}
 
+	public String anchorValue(String value, UsageRule rule) {
+		return substring(value, rule, '#');
+	}
+	
+	public String deedOfEngagementAddendumSubusageValue(String value) {
+		String addendum = anchorValue(value, UsageRule.DEED_OF_ENGAGEMENT);
+		return addendum.substring(0, addendum.length() - _ADDENDUM.length());
+	}
+
+	public String voletCountValue(String value) {
+		return anchorValue(value, UsageRule.VOLET_COUNT);
+	}
+	
 }
